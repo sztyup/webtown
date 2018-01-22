94b2129d3488f258df15cd13dcc8c2092d0b2eb8
Sztyup <sztyup@gmail.com>
2018-01-22 05:53:27 +0100

    Basic products added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,22 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Product;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use ValidatesRequests;
 
     public function index()
     {
+        $products = [
+            new Product('téliszalámi', 2000),
+            new Product('gumikacsa', 3000),
+            new Product('uborka', 2800, true),
+            new Product('gesztenye', 1000, true)
+        ];
 
+        return view('index', [
+            'products' => $products
+        ]);
     }
 
-    public function post()
+    public function post(Request $request)
     {
-
+        return view('index');
     }
 }
