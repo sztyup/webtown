fb8e77ef95575546c11e8de6c0c20be15d2502ec
Sztyup <sztyup@gmail.com>
2018-01-22 08:14:09 +0100

    Seperate layout from actual form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,49 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Simple webshop</title>
+@extends('layout')
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-
-<form action="index.php" method="post">
-@foreach ($products as $product)
-        <div class="form-group {{ $errors->has($product->getId()) ? "has-error" : "" }}">
-            <label for="{{ $product->getId() }}">{{ $product->getName() }}</label>
-            <input
-                    id="{{ $product->getId() }}"
-                    class="form-control"
-                    name="{{ $product->getId() }}"
-                    type="number"
-                    step="1"
-                    value="{{ old(
+@section('content')
+    <form action="index.php" method="post">
+        @foreach ($products as $product)
+            <div class="form-group {{ $errors->has($product->getId()) ? "has-error" : "" }}">
+                <label for="{{ $product->getId() }}">{{ $product->getName() }}</label>
+                <input
+                        id="{{ $product->getId() }}"
+                        class="form-control"
+                        name="{{ $product->getId() }}"
+                        type="number"
+                        step="1"
+                        value="{{ old(
                         $product->getId(),
                         isset($cart) ? $cart->getAmountOf($product) : 0
                     ) }}"
-            >
-            @if ($errors->has($product->getId()))
-            <span class="help-block">
+                >
+                @if ($errors->has($product->getId()))
+                    <span class="help-block">
                 <strong>{{ $errors->first($product->getId()) }}</strong>
             </span>
-            @endif
-        </div>
-@endforeach
-    {!! csrf_field() !!}
-    <button type="submit" class="btn btn-default">Ellenőrzés</button>
-</form>
+                @endif
+            </div>
+        @endforeach
+        {!! csrf_field() !!}
+        <button type="submit" class="btn btn-default">Ellenőrzés</button>
+    </form>
 
-@if (isset($cart))
-    <p>Eredeti ár: {{ $cart->getOriginalPrice() }}</p>
-    @if ($cart->getDiscount())
-    <p>Kedvezményes ár: {{ $cart->getFinalPrice() }}</p>
-    <p>Alkalmazott kedvezmény: {{ $cart->getDiscount()->getName() }}</p>
-    @else
-    <p>Nem alkalmazható semmilyen kedvezmény :/</p>
+    @if (isset($cart))
+        <p>Eredeti ár: {{ $cart->getOriginalPrice() }} HUF ({{ $exchanger->exchange($cart->getOriginalPrice()) }} EUR)</p>
+        @if ($cart->getDiscount())
+            <p>Kedvezményes ár: {{ $cart->getFinalPrice() }} HUF ({{ $exchanger->exchange($cart->getFinalPrice()) }} EUR)</p>
+            <p>Alkalmazott kedvezmény: {{ $cart->getDiscount()->getName() }}</p>
+        @else
+            <p>Nem alkalmazható semmilyen kedvezmény :/</p>
+        @endif
     @endif
-@endif
-</body>
-</html>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simple webshop</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            @yield('content')
+        </div>
+    </div>
+</div>
+</body>
+</html>
