5dd23fad2ff426ee887747cdae1f5dacb7639d4f
Sztyup <sztyup@gmail.com>
2018-01-22 05:07:14 +0100

    Basic view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simple webshop</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<h1>Hello, world!</h1>
+<form action="" method="post">
+
+</form>
+
+</body>
+</html>
