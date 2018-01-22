90b1a52c9ca34fb7206af5fb8decbfd2b5b8921b
Sztyup <sztyup@gmail.com>
2018-01-22 04:17:27 +0100

    No auth

--- config/auth.php
+++ config/auth.php
@@ -65,15 +65,6 @@ return [
     */
 
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => App\User::class,
-        ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
