631b3e2ab000f6e84164cf1b7ce21065a8ea301c
Sztyup <sztyup@gmail.com>
2018-01-22 07:01:53 +0100

    Create form view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,10 +9,38 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<h1>Hello, world!</h1>
-<form action="" method="post">
 
+<form action="index.php" method="post">
+@foreach ($products as $product)
+        <div class="form-group {{ $errors->has($product->getId()) ? "has-error" : "" }}">
+            <label for="{{ $product->getId() }}">{{ $product->getName() }}</label>
+            <input
+                    id="{{ $product->getId() }}"
+                    class="form-control"
+                    name="{{ $product->getId() }}"
+                    type="number"
+                    step="1"
+                    value="{{ old($product->getId(), 0) }}"
+            >
+            @if ($errors->has($product->getId()))
+            <span class="help-block">
+                <strong>{{ $errors->first($product->getId()) }}</strong>
+            </span>
+            @endif
+        </div>
+@endforeach
+    {!! csrf_field() !!}
+    <button type="submit" class="btn btn-default">Ellenőrzés</button>
 </form>
 
+@if (isset($cart))
+    <p>Eredeti ár: {{ $cart->getOriginalPrice() }}</p>
+    @if ($cart->getDiscount())
+    <p>Kedvezményes ár: {{ $cart->getFinalPrice() }}</p>
+    <p>Alkalmazott kedvezmény: {{ $cart->getDiscount()->getName() }}</p>
+    @else
+    <p>Nem alkalmazható semmilyen kedvezmény :/</p>
+    @endif
+@endif
 </body>
 </html>
